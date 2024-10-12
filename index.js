164433cccebd9d43b089723b5d44f6518ee260c3
ANI-RUDH <117719175+ANIi-RUDH@users.noreply.github.com>
2024-10-12 13:50:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import Verify from "./components/yes";
+
+ReactDOM.render(<App />, document.getElementById("root"));
